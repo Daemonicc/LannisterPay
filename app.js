d9932569b925dd6d366fdadfee53c7d403de11bf
daemonicc <igunnuemmanuel@gmail.com>
2022-07-04 16:41:47 +0100

    fixed error

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
 app.post("/split-payments/compute", (req, res) => {
-  console.log("helllo sdasas");
   try {
     const paymentDetails = req.body;
 
